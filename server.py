e88947839b572412cd16f626ea77b73712ffc035
Your Name <you@example.com>
2016-05-09 14:11:27 -0700

    Added session to server.py

--- server.py
+++ server.py
@@ -1,24 +1,45 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, session, render_template, request, redirect, g, url_for
+import os
 
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-app = Flask (__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        session.pop('user', None)
 
-app.secret_key = "ABC"
+        if request.form['password'] == 'password':
+            session['user'] = request.form['username']
+            return redirect(url_for('protected'))
 
-@app.route("/")
-def homepage():
-	"""This is the homepage."""
+    return render_template('index.html')
 
-	return render_template ("homepage.html")
+@app.route('/protected')
+def protected():
+    if g.user:
+        return render_template('protected.html')
 
+    return redirect(url_for('index'))
 
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
 
+@app.route('/getsession')
+def getsession():
+    if 'user' in session:
+        return session['user']
 
+    return 'Not logged in!'
 
+@app.route('/dropsession')
+def dropsession():
+    session.pop('user', None)
+    return 'Dropped!'
 
-
-
-
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
